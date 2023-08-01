bf214cf6c8702b9bc2351efe7998df128a28209d
ROBIN MULLER <robinfmuller@gmail.com>
2023-08-01 12:03:49 +0100

    Add function to fetch data from data.json

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,17 @@
 function startGame() {
     let button = document.getElementById("start-button");
     button.style.backgroundColor = "pink";
+
+    fetchData("../assets/js/data.json");
+}
+
+async function fetchData(url) {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        console.log(data[0].img);
+    } catch (error) {
+        console.log(error);
+    }
 }
